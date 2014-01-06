f4835f2c4b00eb4d364e149dffd19c7c47c6dc5e
Dejan Sakelska <dejan.sakelsak@gmail.com>
2014-01-06 01:10:53 +0100

    Works like a charm

--- springmvc/src/main/java/org/zabica/webcontest/springmvc/WebContest.java
+++ springmvc/src/main/java/org/zabica/webcontest/springmvc/WebContest.java
@@ -1,18 +1,30 @@
 package org.zabica.webcontest.springmvc;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+import org.zabica.webcontest.common.user.User;
 
 @Controller
 @RequestMapping("/")
 public class WebContest {
 
 	@RequestMapping("webcontest")
-    public Object webcontest() {
-		
+	public Object webcontest(@RequestParam(required = false) String name,
+			HttpServletRequest servletRequest,
+			HttpServletResponse servletResponse) {
+
 		ModelAndView mav = new ModelAndView("index");
-    	return mav;
-    }
+		if (name != null) {
+			User user = new User();
+			user.setFirstName(name);
+			mav.addObject("user", user);
+		}
+		return mav;
+	}
 }
