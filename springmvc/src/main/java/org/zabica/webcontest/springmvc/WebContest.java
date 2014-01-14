1280c9063113e635febce6f4ea4e4b71d06089f4
Dejan Sakelska <dejan.sakelsak@gmail.com>
2014-01-14 01:40:37 +0100

    further development

--- springmvc/src/main/java/org/zabica/webcontest/springmvc/WebContest.java
+++ springmvc/src/main/java/org/zabica/webcontest/springmvc/WebContest.java
@@ -5,16 +5,19 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.zabica.webcontest.common.user.User;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/webcontest")
 public class WebContest {
 
-	@RequestMapping("webcontest")
+	private static final String up = "bla";
+	
+	@RequestMapping(method = RequestMethod.GET)
 	public Object webcontest(@RequestParam(required = false) String name,
 			HttpServletRequest servletRequest,
 			HttpServletResponse servletResponse) {
@@ -26,5 +29,16 @@ public class WebContest {
 			mav.addObject("user", user);
 		}
 		return mav;
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	@ResponseBody
+	public Object login(@RequestParam(required=true) String username, @RequestParam(required=true) String password) {
+		
+		if(username.equals(up) && password.equals(up)) {
+			return "You're in!";
+		}
+		
+		return "You're out!";
 	}
 }
